a8bcbc558f43439e517a741ee912d578c663ebec
Frederico Jordan <fredericojordan@gmail.com>
2018-10-25 23:33:49 -0300

    Fixing import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './Home';
+import Home from './home';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
