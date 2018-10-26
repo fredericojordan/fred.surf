a40ba16ec5459bd3fe1f9172defbd60d9ef46135
Frederico Jordan <fredericojordan@gmail.com>
2018-10-26 19:11:45 -0300

    Adding instagram link

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 
 const Contact = props => (
-    <a href="https://github.com/fredericojordan">
-        <img src="/images/github.png" alt="My github" />
-    </a>
+    <div>
+        <a href="https://github.com/fredericojordan">
+            <img src="/images/github.png" alt="My github" />
+        </a>
+        <a href="https://www.instagram.com/fredericojordan/">
+            <img src="/images/instagram.png" alt="My instagram" />
+        </a>
+    </div>
 )
 
 
