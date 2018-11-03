abf1b279def86af3641911f35aa936ae612bc747
Frederico Jordan <fredericojordan@gmail.com>
2018-11-03 11:51:18 -0300

    Adding Linkedin link

--- src/components/molecules/contacts/index.js
+++ src/components/molecules/contacts/index.js
@@ -7,6 +7,7 @@ import './styles.css'
 const Contacts = props => (
     <div className="Contacts">
         <Contact target="https://github.com/fredericojordan/" image="/images/github.png" alt="My github" />
+        <Contact target="https://www.linkedin.com/in/frederico-jordan/" image="/images/linkedin.png" alt="My linkedin" />
         <Contact target="https://www.instagram.com/fredericojordan/" image="/images/instagram.png" alt="My instagram" />
     </div>
 );
