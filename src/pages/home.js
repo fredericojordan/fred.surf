20d64ead6c2ca343c1fe98befc38c0a58e297aed
Frederico Jordan <fredericojordan@gmail.com>
2018-11-03 12:27:00 -0300

    Renaming CSS

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,8 +6,8 @@ import Contacts from '../components/molecules/contacts'
 class Home extends Component {
     render() {
         return (
-            <div className="Home">
-                <header className="Home-header">
+            <div className="home">
+                <header className="home__header">
                     <Contacts />
                 </header>
             </div>
