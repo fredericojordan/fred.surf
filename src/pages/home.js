8571dfe96791d6fd0c9bc2f5722b4f1be37c8c61
Frederico Jordan <fredericojordan@gmail.com>
2018-11-09 11:17:17 -0200

    Adding instagram embed to home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import InstagramEmbed from 'react-instagram-embed'
+
+import './home.css'
 
 
 class Home extends Component {
     render() {
         return (
             <div className="home">
+                <InstagramEmbed
+                    url='https://instagr.am/p/BISbbi6jw7z/'
+                    maxWidth={640}
+                    hideCaption={false}
+                    containerTagName='div'
+                    protocol=''
+                    injectScript
+                    onLoading={() => {}}
+                    onSuccess={() => {}}
+                    onAfterRender={() => {}}
+                    onFailure={() => {}}
+                />
             </div>
         );
     }
