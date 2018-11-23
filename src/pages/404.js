7acba6c443c47758ca9629193c767c4fb787c2c5
Frederico Jordan <fredericojordan@gmail.com>
2018-11-23 00:16:35 -0200

    Removing 404 status attempt

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,29 +1,17 @@
 import React, { Component } from 'react';
-import { Route } from "react-router-dom";
 
 import './home.css'
-
-const Status = ({ code, children }) => (
-  <Route
-    render={({ staticContext }) => {
-      if (staticContext) staticContext.status = code;
-      return children;
-    }}
-  />
-);
 
 class Page404 extends Component {
     render() {
         return (
-            <Status code={404}>
-                <div className="home">
-                    <div className="home__text">
-                        Error 404
-                        <br />
-                        Page not found!
-                    </div>
+            <div className="home">
+                <div className="home__text">
+                    Error 404
+                    <br />
+                    Page not found!
                 </div>
-            </Status>
+            </div>
         );
     }
 }
