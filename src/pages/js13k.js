2254b47f293bb9f499cdc4cb6c140dd21017a385
Frederico Jordan <fredericojordan@gmail.com>
2019-08-15 02:31:10 -0300

    Adding js13k page

---
+++ src/pages/js13k.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class JS13k extends Component {
+    render = () => <div id="js13k" style={{margin: 20}}></div>
+
+    componentDidMount () {
+        const script = document.createElement("script");
+
+        script.src = "/js13k-min.js";
+        script.async = true;
+
+        document.body.appendChild(script);
+    }
+}
+
+export default JS13k;
